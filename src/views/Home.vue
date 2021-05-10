75cbe7dd8d8b7633153bcc1f4b40abbc3c1c1b59
mounwater <mounfxgwater@163.com>
2021-05-10 20:14:34 +0800

    完成首页

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,13 +24,205 @@
           :to="{ name: 'Hot', params: { id: c._id } }"
         >
         </van-grid-item>
-        <div v-for="c in categories" :key="c._id">
-          <p>
-            <span>{{ c.name }}</span
-            ><router-link :to="{ name: 'Hot', params: { id: c._id } }"
-              >更多</router-link
+        <div class="showthere">
+          <p class="showtitle">
+            <span class="fenlei">手机</span
+            ><router-link
+              :to="{ name: 'Hot', params: { id: '60923bd00cac5712dc34e5ee' } }"
+              >更多&gt;</router-link
             >
           </p>
+          <div class="showbottom">
+            <ul>
+              <li class="card" v-for="phoneCom in phone" :key="phoneCom._id">
+                <router-link
+                  :to="{
+                    name: 'Detail',
+                    params: {
+                      id: phoneCom._id,
+                    },
+                  }"
+                >
+                  <img :src="phoneCom.coverImg" alt="" />
+                  <p>
+                    {{ phoneCom.name }}
+                  </p>
+                  <p>
+                    <span class="red">{{ phoneCom.price }}</span
+                    >元
+                  </p>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="showthere">
+          <p class="showtitle">
+            <span class="fenlei">智能手表</span
+            ><router-link
+              :to="{ name: 'Hot', params: { id: '60923bd00cac5712dc34e5ef' } }"
+              >更多&gt;</router-link
+            >
+          </p>
+          <div class="showbottom">
+            <ul>
+              <li class="card" v-for="watchCom in watches" :key="watchCom._id">
+                <router-link
+                  :to="{
+                    name: 'Detail',
+                    params: {
+                      id: watchCom._id,
+                    },
+                  }"
+                >
+                  <img :src="watchCom.coverImg" alt="" />
+                  <p>
+                    {{ watchCom.name }}
+                  </p>
+                  <p>
+                    <span class="red">{{ watchCom.price }}</span
+                    >元
+                  </p>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="showthere">
+          <p class="showtitle">
+            <span class="fenlei">耳机</span
+            ><router-link
+              :to="{ name: 'Hot', params: { id: '60923bd00cac5712dc34e5f0' } }"
+              >更多&gt;</router-link
+            >
+          </p>
+          <div class="showbottom">
+            <ul>
+              <li
+                class="card"
+                v-for="headsetCom in headset"
+                :key="headsetCom._id"
+              >
+                <router-link
+                  :to="{
+                    name: 'Detail',
+                    params: {
+                      id: headsetCom._id,
+                    },
+                  }"
+                >
+                  <img :src="headsetCom.coverImg" alt="" />
+                  <p>
+                    {{ headsetCom.name }}
+                  </p>
+                  <p>
+                    <span class="red">{{ headsetCom.price }}</span
+                    >元
+                  </p>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="showthere">
+          <p class="showtitle">
+            <span class="fenlei">电视</span
+            ><router-link
+              :to="{ name: 'Hot', params: { id: '60923bd00cac5712dc34e5f1' } }"
+              >更多&gt;</router-link
+            >
+          </p>
+          <div class="showbottom">
+            <ul>
+              <li class="card" v-for="tvCom in tv" :key="tvCom._id">
+                <router-link
+                  :to="{
+                    name: 'Detail',
+                    params: {
+                      id: tvCom._id,
+                    },
+                  }"
+                >
+                  <img :src="tvCom.coverImg" alt="" />
+                  <p>
+                    {{ tvCom.name }}
+                  </p>
+                  <p>
+                    <span class="red">{{ tvCom.price }}</span
+                    >元
+                  </p>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="showthere">
+          <p class="showtitle">
+            <span class="fenlei">灯具</span
+            ><router-link
+              :to="{ name: 'Hot', params: { id: '60923bd00cac5712dc34e5f2' } }"
+              >更多&gt;</router-link
+            >
+          </p>
+          <div class="showbottom">
+            <ul>
+              <li class="card" v-for="lightCom in light" :key="lightCom._id">
+                <router-link
+                  :to="{
+                    name: 'Detail',
+                    params: {
+                      id: lightCom._id,
+                    },
+                  }"
+                >
+                  <img :src="lightCom.coverImg" alt="" />
+                  <p>
+                    {{ lightCom.name }}
+                  </p>
+                  <p>
+                    <span class="red">{{ lightCom.price }}</span
+                    >元
+                  </p>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="showthere">
+          <p class="showtitle">
+            <span class="fenlei">口红</span
+            ><router-link
+              :to="{ name: 'Hot', params: { id: '60923bd00cac5712dc34e5f3' } }"
+              >更多&gt;</router-link
+            >
+          </p>
+          <div class="showbottom">
+            <ul>
+              <li
+                class="card"
+                v-for="lipstickCom in lipstick"
+                :key="lipstickCom._id"
+              >
+                <router-link
+                  :to="{
+                    name: 'Detail',
+                    params: {
+                      id: lipstickCom._id,
+                    },
+                  }"
+                >
+                  <img :src="lipstickCom.coverImg" alt="" />
+                  <p>
+                    {{ lipstickCom.name }}
+                  </p>
+                  <p>
+                    <span class="red">{{ lipstickCom.price }}</span
+                    >元
+                  </p>
+                </router-link>
+              </li>
+            </ul>
+          </div>
         </div>
       </van-grid>
     </div>
@@ -49,15 +241,36 @@ export default {
     return {
       list: [],
       categories: [],
-      products: [],
+      phone: [],
+      watches: [],
+      headset: [],
+      tv: [],
+      light: [],
+      lipstick: [],
     };
   },
-  async created() {
-    const c = await loadCategories();
-    const p = await loadProducts();
-    console.log(c);
-    this.categories = c.categories;
-    this.products = p.products;
+  created() {
+    loadCategories().then((res) => {
+      this.categories = res.categories;
+    });
+    loadProducts(1, '60923bd00cac5712dc34e5ee').then((res) => {
+      this.phone = res.products;
+    });
+    loadProducts(1, '60923bd00cac5712dc34e5ef').then((res) => {
+      this.watches = res.products;
+    });
+    loadProducts(1, '60923bd00cac5712dc34e5f0').then((res) => {
+      this.headset = res.products;
+    });
+    loadProducts(1, '60923bd00cac5712dc34e5f1').then((res) => {
+      this.tv = res.products;
+    });
+    loadProducts(1, '60923bd00cac5712dc34e5f2').then((res) => {
+      this.light = res.products;
+    });
+    loadProducts(1, '60923bd00cac5712dc34e5f3').then((res) => {
+      this.lipstick = res.products;
+    });
   },
   mounted() {
     var getPic = async () => {
@@ -125,5 +338,39 @@ h3 {
 .tips {
   color: skyblue;
   font-size: 0.2rem;
+}
+.showthere {
+  width: 100%;
+  min-height: 30vh;
+  border-top: 1px solid gray;
+}
+.showtitle {
+  display: flex;
+  justify-content: space-between;
+  padding: 0 1rem;
+  line-height: 2rem;
+  font-size: 0.9rem;
+}
+.showbottom {
+  overflow-x: auto;
+}
+.showbottom ul {
+  min-width: 1500px;
+  display: flex;
+  justify-content: space-around;
+}
+.showbottom li {
+  width: 8%;
+  font-size: 0.5rem;
+  padding: 0.6rem 0;
+}
+.card img {
+  width: 100%;
+}
+.red {
+  color: red;
+}
+.fenlei {
+  font-weight: bold;
 }
 </style>
