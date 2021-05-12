aa9aeb9883af14b9595a4321f9972d28a05e682b
TH <842973926@qq.com>
2021-05-12 11:07:35 +0800

    删除商品，提交订单

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -2,7 +2,7 @@
   <!-- 购物车 -->
 
   <div class="about">
-    <van-cell-group>
+    <van-cell-group style="padding-bottom:20px;">
       <van-cell v-for="(i, index) in carts" :key="i._id">
         <van-checkbox v-model="i.checked"></van-checkbox>
         <van-card
@@ -10,9 +10,10 @@
           :title="i.product.name"
           :thumb="i.product.coverImg"
         >
+          <template> </template>
           <template #footer>
             <span class="singlePrice"
-              >合计:{{ i.product.price * i.quantity }}</span
+              >￥{{ i.product.price * i.quantity }}</span
             >
             <div class="th">
               <van-button
@@ -28,6 +29,9 @@
                 icon="plus"
                 type="danger"
               ></van-button>
+              <van-button type="danger" size="small" @click="del(i._id, index)">
+                删除
+              </van-button>
             </div>
           </template>
         </van-card>
@@ -38,7 +42,7 @@
       button-text="提交订单"
       @submit="onSubmit"
     >
-      <van-checkbox v-model="checkedAll">全选</van-checkbox>
+      <van-checkbox v-model="checkedAll" class="">全选</van-checkbox>
       <template #tip>
         你的收货地址不支持同城送,
         <span @click="onClickEditAddress" style="color:blue;">修改地址</span>
@@ -49,14 +53,20 @@
 
 <script>
 import { loadCarts, addToCart } from "../services/carts";
+import { delCarts } from "../services/carts";
+import { tjdd } from "../services/carts";
+import { cxaddress } from "../services/carts";
 export default {
   data() {
     return {
       carts: [],
+      location: [],
+      carted: [],
     };
   },
   async created() {
     this.loadData();
+    this.address();
   },
   methods: {
     async changeCartData(p, q, index) {
@@ -71,8 +81,46 @@ export default {
       this.carts = res.map((item) => ({ ...item, checked: false }));
       console.log(this.carts);
     },
-    onSubmit() {},
-    onClickEditAddress() {},
+    async address() {
+      const res = await cxaddress();
+      this.location = res.addresses.filter((item) => item.isDefault == true);
+      // console.log(res.addresses.filter((item) => item.isDefault == true));
+      // this.location.forEach((item) => console.log(item.address));
+    },
+    del(id, index) {
+      delCarts(id);
+      this.carts[index].checked = false;
+      this.carts.splice(index, 1);
+      if (this.carts.length == 0) {
+        this.$refs.dom.checked = false;
+      }
+      console.log(this.$refs.dom.length);
+    },
+    onSubmit() {
+      this.carts
+        .filter((item) => item.checked)
+        .forEach((item) =>
+          this.carted.push({
+            quantity: item.quantity,
+            product: item.product._id,
+            price: item.product.price,
+          })
+        );
+
+      console.log(this.carted);
+      /* this.location.forEach((item) =>
+        tjdd(item.receiver, item.regions, item.address)
+      ); */
+      tjdd(
+        this.location[0].receiver,
+        this.location[0].regions,
+        this.location[0].address,
+        this.carted
+      ).then((res) => console.log(res.code));
+    },
+    onClickEditAddress() {
+      this.$router.push("Address");
+    },
   },
   computed: {
     checkedAll: {
@@ -80,7 +128,15 @@ export default {
         this.carts.forEach((item) => (item.checked = v));
       },
       get() {
-        return this.carts.every((item) => item.checked);
+        if (this.carts.length != 0) {
+          return (
+            this.carts.length ==
+            this.carts.filter((item) => item.checked).length
+          );
+        } else {
+          return false;
+        }
+        // return this.carts.every((item) => item.checked);
       },
     },
     sumprice() {
